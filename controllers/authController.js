21ea5695c23d6cab17e6740b239c00716b4798e9
. <barcalover016@gmail.com>
2025-02-09 23:01:38 +0100

    some changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -24,8 +24,7 @@ const login = async (req, res) => {
 
   const token = jwt.sign(
    { id: user._id, role: user.role, profilePicture: user.profilePicture },
-   process.env.JWT_SECRET,
-   { expiresIn: "1h" }
+   process.env.JWT_SECRET
   );
   res.json({
    token,
